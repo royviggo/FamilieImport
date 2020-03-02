7a6908c42ddbd0e01293c508d9ca757d617eed38
royviggo <royviggo@gmail.com>
2020-03-02 23:06:22 +0100

    Added record types to GedcomDocument

--- FamilieImport.Gedcom/Models/GedcomDocument.cs
+++ FamilieImport.Gedcom/Models/GedcomDocument.cs
@@ -1,17 +1,25 @@
-﻿using System.Collections.Generic;
+﻿using FamilieImport.Gedcom.Structures;
+using System.Collections.Generic;
 
 namespace FamilieImport.Gedcom.Models
 {
     public class GedcomDocument
     {
-        //private readonly Dictionary<GedcomId, GedcomRecord> _xReferences = new Dictionary<GedcomId, GedcomRecord>();
-        //private readonly List<GedcomRecord> _gedcomRecords = new List<GedcomRecord>();
-
         public GedcomDocument()
         {
         }
 
         public Dictionary<GedcomXref, GedcomRecord> CrossReferences { get; set; }
         public List<GedcomRecord> GedcomRecords { get; set; }
+
+        public HeaderRecord HeaderRecord { get; set; }
+        public SubmissionRecord SubmissionRecord { get; set; }
+        public List<FamilyRecord> FamilyRecords { get; set; }
+        public List<IndividualRecord> IndividualRecords { get; set; }
+        public List<MultimediaRecord> MultimediaRecords { get; set; }
+        public List<NoteRecord> NoteRecords { get; set; }
+        public List<RepositoryRecord> RepositoryRecords { get; set; }
+        public List<SourceRecord> SourceRecords { get; set; }
+        public List<SubmitterRecord> SubmitterRecords { get; set; }
     }
 }
