b8ca8804001809e0da84952b40cd7959ae349629
royviggo <royviggo@gmail.com>
2020-06-13 11:00:00 +0200

    Added GedcomLineCollection as a replacement for GedcomRecord

---
+++ FamilieImport.Gedcom/Models/GedcomLineCollection.cs
@@ -0,0 +1,55 @@
+﻿using FamilieImport.Gedcom.Enums;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FamilieImport.Gedcom.Models
+{
+    public class GedcomLineCollection : GedcomLine
+    {
+        public GedcomLineCollection()
+        {
+            GedcomLines = new List<GedcomLine>();
+        }
+
+        public GedcomLineCollection(string line) : base(line)
+        {
+            GedcomLines = new List<GedcomLine>();
+        }
+
+        public GedcomLineCollection(GedcomLine gedcomLine) : base(gedcomLine)
+        {
+            GedcomLines = new List<GedcomLine>();
+        }
+
+        public GedcomLineCollection(int level, string id, string tag, string value, string pointer) : base(level, id, tag, value, pointer)
+        {
+            GedcomLines = new List<GedcomLine>();
+        }
+
+        public List<GedcomLine> GedcomLines { get; set; }
+
+
+        public void Clear()
+        {
+            Level = 0;
+            Id = null;
+            Tag = GedcomTag.UNKNOWN;
+            Value = "";
+            Pointer = null;
+            GedcomLines.Clear();
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.Append(base.ToString());
+
+            foreach (var line in GedcomLines)
+            {
+                sb.AppendLine(line.ToString());
+            }
+
+            return sb.ToString();
+        }
+    }
+}
